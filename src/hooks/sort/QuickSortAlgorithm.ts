fc8b9e59a893e58f974234937fe2d5c546b21e1e
Ali God <aligod8001@gmail.com>
2023-11-19 11:32:57 +0330

    [IMP] implement descending order of sort for quick-sort alg.

--- src/hooks/sort/QuickSortAlgorithm.ts
+++ src/hooks/sort/QuickSortAlgorithm.ts
@@ -2,47 +2,49 @@ let sortSteps: number[][] = [];
 let sortColors: number[][] = [];
 
 const swap = (arr: number[], a: number, b: number) => {
-    const temp = arr[a]
-    arr[a] = arr[b]
-    arr[b] = temp
-}
+  const temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+};
 
-const partition = (arr: number[], l: number, h: number) => {
-    const pivot = arr[h]
-    let i = l -1
-    
-    for (let j = l; j <= h; j++) {
-        if ( arr[j] < pivot ) {
-            i++
-            swap(arr, i, j)
-            sortColors.push([i, j])
-            sortSteps.push([...arr])
-        }
+const partition = (arr: number[], l: number, h: number, desc: boolean) => {
+  const pivot = arr[h];
+  let i = l - 1;
+
+  const compare = desc ? (a: number, b: number) => a > b : (a: number, b: number) => a < b;
+
+  for (let j = l; j <= h; j++) {
+    if (compare(arr[j], pivot)) {
+      i++;
+      swap(arr, i, j);
+      sortColors.push([i, j]);
+      sortSteps.push([...arr]);
     }
-    
-    swap(arr, i+1, h)
-    sortColors.push([i+1, h])
-    sortSteps.push([...arr])
-    return i + 1
-}
+  }
 
-const QuickSort = (arr: number[], l: number, h:number) => {
-    if (l < h) {
-        const p = partition(arr, l, h)
-        QuickSort(arr, l, p-1)
-        QuickSort(arr, p+1, h)
-    }
-}
+  swap(arr, i + 1, h);
+  sortColors.push([i + 1, h]);
+  sortSteps.push([...arr]);
+  return i + 1;
+};
 
-const QuickSortAlgorithm = (arr: number[]) => {
-    sortSteps = []
-    sortColors = []
-    sortSteps.push([...arr])
-    sortColors.push([])
-    QuickSort(arr, 0, arr.length - 1)
-    
-    sortColors.push([])
-    return { arr, sortSteps, sortColors }
-}
+const QuickSort = (arr: number[], l: number, h: number, desc: boolean) => {
+  if (l < h) {
+    const p = partition(arr, l, h, desc);
+    QuickSort(arr, l, p - 1, desc);
+    QuickSort(arr, p + 1, h, desc);
+  }
+};
+
+const QuickSortAlgorithm = (arr: number[], desc: boolean) => {
+  sortSteps = [];
+  sortColors = [];
+  sortSteps.push([...arr]);
+  sortColors.push([]);
+  QuickSort(arr, 0, arr.length - 1, desc);
+
+  sortColors.push([]);
+  return { arr, sortSteps, sortColors };
+};
 
 export default QuickSortAlgorithm;
